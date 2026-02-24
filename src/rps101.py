5a7cf41e65d3ce53d3667ba5d29be6acd02fd28b
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 12:16:56 +0300

    Update rps101.py

--- src/rps101.py
+++ src/rps101.py
@@ -1,29 +1,22 @@
-from requests import get
+from requests import Session
 
-class RPS101:
+class Rps101:
 	def __init__(self) -> None:
 		self.api = "https://rps101.pythonanywhere.com/api/v1"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
 		}
 
-
 	def get_all_objects(self) -> dict:
-		return get(
-			f"{self.api}/objects/all",
-			headers=self.headers).json()
-
+		return self.session.get(f"{self.api}/objects/all").json()
 
 	def get_object_info(self, object_name: str) -> dict:
-		return get(
-			f"{self.api}/objects/{object_name}",
-			headers=self.headers).json()
-
+		return self.session.get(f"{self.api}/objects/{object_name}").json()
 
 	def get_match_result(
 			self,
 			object_one: str,
 			object_two: str) -> dict:
-		return get(
-			f"{self.api}/match?object_one={object_one}&object_two={object_two}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/match?object_one={object_one}&object_two={object_two}").json()
